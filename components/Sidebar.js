d36657dabffbdf8b65a26090ff2f93bfb46d4ea6
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-17 17:51:54 +0200

    update sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,26 +1,41 @@
-import { HiHome, HiSearch, HiOutlineMenuAlt1, HiOutlinePlus } from "react-icons/hi";
+import {
+  HiHome,
+  HiSearch,
+  HiOutlineMenuAlt1,
+  HiOutlinePlus,
+  HiHeart,
+} from "react-icons/hi";
 
 export default function Sidebar() {
   return (
-    <div>
-      <div>
-        <button className="flex items-center space-x-2">
+    <div className="text-gray-400 p-5 text-sm border-r border-gray-900">
+      <div className="space-y-2">
+        <button className="flex items-center space-x-2 hover:text-white">
           <HiHome className="h-6 w-6" />
           <p>Accueil</p>
         </button>
-        <button className="flex items-center space-x-2">
+        <button className="flex items-center space-x-2 hover:text-white">
           <HiSearch className="h-6 w-6" />
           <p>Rechercher</p>
         </button>
-        <button className="flex items-center space-x-2">
+        <button className="flex items-center space-x-2 hover:text-white">
           <HiOutlineMenuAlt1 className="h-6 w-6" />
           <p>Bibliothèque</p>
         </button>
-        <button className="flex items-center space-x-2">
+      </div>
+      <div className="space-y-2">
+        <button className="flex items-center space-x-2 hover:text-white mt-5">
           <HiOutlinePlus className="h-6 w-6" />
           <p>Créer une playlist</p>
         </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HiHeart className="h-6 w-6" />
+          <p>Titres likés</p>
+        </button>
       </div>
+      <hr className="border-t-[0.1px] border-gray-400 mt-5" />
+
+      {/* Playlists */}
     </div>
   );
 }
