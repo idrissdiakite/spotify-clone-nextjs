2ab26cbc856b9341abc0a8da5bf73e966f52bd3c
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-17 17:35:57 +0200

    add react-icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,26 @@
-function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
-}
+import { HiHome, HiSearch, HiOutlineMenuAlt1, HiOutlinePlus } from "react-icons/hi";
 
-export default Sidebar
+export default function Sidebar() {
+  return (
+    <div>
+      <div>
+        <button className="flex items-center space-x-2">
+          <HiHome className="h-6 w-6" />
+          <p>Accueil</p>
+        </button>
+        <button className="flex items-center space-x-2">
+          <HiSearch className="h-6 w-6" />
+          <p>Rechercher</p>
+        </button>
+        <button className="flex items-center space-x-2">
+          <HiOutlineMenuAlt1 className="h-6 w-6" />
+          <p>Bibliothèque</p>
+        </button>
+        <button className="flex items-center space-x-2">
+          <HiOutlinePlus className="h-6 w-6" />
+          <p>Créer une playlist</p>
+        </button>
+      </div>
+    </div>
+  );
+}
