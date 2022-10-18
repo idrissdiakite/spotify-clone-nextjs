4ab3adf2174e17ed1f79ffec07354d05ec8fc7ed
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-18 19:01:11 +0200

    set up login page

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { getProviders, signIn } from "next-auth/react";
 
-function Login() {
+function Login({ providers }) {
+  console.log(providers)
   return (
-    <div>
-    <h1>Se connecter</h1>
+    <div className="flex items-center bg-black min-h-screen w-full justify-center">
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-lg"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" } )}
+          >
+            Se connecter avec {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
