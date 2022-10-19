7826ca477f77dc2c609ab8e353f1d0bb9653578c
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-19 23:46:16 +0200

    add sessionProvider

--- pages/login.js
+++ pages/login.js
@@ -2,14 +2,14 @@ import React from "react";
 import { getProviders, signIn } from "next-auth/react";
 
 function Login({ providers }) {
-  console.log(providers)
+  console.log(providers);
   return (
     <div className="flex items-center bg-black min-h-screen w-full justify-center">
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
             className="bg-[#18D860] text-white p-5 rounded-lg"
-            onClick={() => signIn(provider.id, { callbackUrl: "/" } )}
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
             Se connecter avec {provider.name}
           </button>
