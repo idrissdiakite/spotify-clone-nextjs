27a2ade3b7b2dd7b7631547d3eea2bfed3157890
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-20 22:26:33 +0200

    display playlist cover & name

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
 
+export const playlistState = atom({
+  key: "playlistState",
+  default: "null",
+});
+
 export const playlistIdState = atom({
   key: "playlistIdState", // needs to be unique
   default: "42CVOw2LSMXDgi4nw0KT0F", // = HIPHOP
--- components/Content.js
+++ components/Content.js
@@ -1,10 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from 'react';
-import { useRecoilValue } from "recoil";
+import { useEffect, useState } from "react";
+import { useRecoilValue, useRecoilState } from "recoil";
 import { HiChevronDown } from "react-icons/hi";
-import { shuffle } from "lodash"
-import { playlistIdState } from '../atoms/playlistAtom';
+import { shuffle } from "lodash";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
 
 const colors = [
   "from-blue-500",
@@ -16,33 +17,63 @@ const colors = [
   "from-red-500",
 ];
 
-
 export default function Content() {
   const { data: session } = useSession();
+  const spotifyApi = useSpotify();
   const [color, setColor] = useState(null);
-  const playlistId = useRecoilValue(playlistIdState); // Read only version
   // const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
+  const playlistId = useRecoilValue(playlistIdState); // Read only version
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
 
   useEffect(() => {
-    // shuffle array of colors and choose one color 
+    // shuffle array of colors and choose one color
     setColor(shuffle(colors).pop());
-  }, [playlistId])
+  }, [playlistId]);
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi
+      .getPlaylist(playlistId)
+      .then((data) => {
+        setPlaylist(data.body);
+        // console.log(data.body)
+      })
+      .catch((err) => console.log("Something went wrong", err));
+    }
+  }, [session, spotifyApi, playlistId, setPlaylist]);
 
   return (
     <div className="flex-grow">
       <header className="absolute top-5 right-8">
         <div className="flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
-        <Image className="rounded-full w-10 h-10"
+          <Image
+            className="rounded-full w-10 h-10"
             src={session?.user.image}
             width={30}
             height={30}
             layout="fixed"
-            alt="Logo" />
+            alt="Logo"
+          />
           <h2>{session?.user.name}</h2>
           <HiChevronDown className="h-6 w-6" />
         </div>
       </header>
-      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 tex-white p-8`}>
+      <section
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 tex-white p-8`}
+      >
+        <Image
+            className="w-44 h-44"
+            src={playlist?.images?.[0]?.url}
+            width={200}
+            height={200}
+            layout="fixed"
+            priority
+            alt="Logo"
+          />
+          <div className="text-white">
+            <p>PLAYLIST</p>
+            <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
+          </div>
       </section>
     </div>
   );
