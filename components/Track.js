459cd4c47f621b581f4514555eb779d42f8a98c1
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-21 01:14:47 +0200

    ui

--- components/Track.js
+++ components/Track.js
@@ -8,25 +8,25 @@ export default function Song({ order, track }) {
   console.log(track);
 
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2 text-gray-400 hover:text-white">
       <div className="flex items-center space-x-4">
         <p className="mr-4">{order + 1}</p>
         <Image
           className="w-10 h-10"
           src={track?.album?.images[0].url}
-          width={80}
-          height={80}
+          width={50}
+          height={50}
           layout="fixed"
           alt=""
         />
         <div>
-          <p>{track.artists[0].name}</p>
-          <p>{track.name}</p>
+          <p className="w-36 lg:w-64 truncate text-white">{track.name}</p>
+          <p className="">{track.artists[0].name}</p>
         </div>
       </div>
       <div className="flex items-center justify-between ml-auto md:ml-0">
         <p className="hidden md:inline">{track.album.name}</p>
-        <p>{millisToMinutesAndSeconds(track.duration_ms)}</p>
+        <p className="text-gray-400">{millisToMinutesAndSeconds(track.duration_ms)}</p>
       </div>
     </div>
   );
