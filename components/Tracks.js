af1c2e31878aed0916c9c9558df743b4cee436ab
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-21 01:19:33 +0200

    ui

--- components/Tracks.js
+++ components/Tracks.js
@@ -7,7 +7,7 @@ export default function Tracks() {
   const playlists = useRecoilValue(playlistState);
 
   return (
-    <div className="flex flex-col space-y-1 pb-28 px-8 text-white">
+    <div className="flex flex-col space-y-3 pb-28 px-8 text-white">
       {playlists?.tracks?.items.map((track, i) => (
         <Track key={track.track.id} track={track.track} order={i} />
       ))}
