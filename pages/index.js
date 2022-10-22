0c38ca16b3814061ed15b671265fe873b8381d00
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-22 22:05:55 +0200

    add player on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Content from '../components/Content'
+import Player from '../components/Player'
 import Sidebar from '../components/Sidebar'
 
 export default function Home() {
@@ -15,6 +16,9 @@ export default function Home() {
         <Sidebar />
         <Content />
       </main>
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
     </div>
   )
 }
