ec1e12e1ca2be1a17ab795678bfef9dfcc27f623
idrissdiakite <idriss.diakite@hotmail.fr>
2022-10-22 22:11:45 +0200

    set up player

--- components/Player.js
+++ components/Player.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import useSpotify from "../hooks/useSpotify";
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { useSession } from "next-auth/react";
+import { currentTrackIdState, isPlayingState } from "../atoms/trackAtom";
 
 export default function Player() {
+
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
+
   return (
     <div>
-      <h1>Player</h1>
+    {/* Cover */}
+      <div>
+
+      </div>
     </div>
   )
 }
