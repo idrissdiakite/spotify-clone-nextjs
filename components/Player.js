861e894d4b51c8e178bbc1a8576b691192f9fc2d
idrissdiakite <idriss.diakite@hotmail.fr>
2022-11-01 13:37:48 +0100

    edit comment player display

--- components/Player.js
+++ components/Player.js
@@ -39,7 +39,7 @@ export default function Player() {
 
   return (
     <div>
-      {/* Cover */}
+      {/* LEFT/COVER */}
       {/* Show player only if track is defined */}
       <div className={track ? "flex items-center space-x-5 bg-gradient-to-b from-black to-gray-900 h-35 p-5 text-white" : ""}>
         <Image
