bdf6fdd2bd05df0042c2c1d5a80786b8e8915e82
idrissdiakite <idriss.diakite@hotmail.fr>
2022-11-04 09:29:23 +0100

    indent

--- components/Track.js
+++ components/Track.js
@@ -4,21 +4,24 @@ import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/trackAtom";
 import { millisToMinutesAndSeconds } from "../lib/time";
 
-export default function Song({ order, track }) { // get order and track from components/tracks
+export default function Song({ order, track }) {
+  // get order and track from components/tracks
   const spotifyApi = useSpotify();
-  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
 
   const playTrack = () => {
     setCurrentTrackId(track.id);
     setIsPlaying(true);
-    spotifyApi.play({uris: [track.uri],
-    });
+    spotifyApi.play({ uris: [track.uri] });
   };
 
-
   return (
-    <div onClick={playTrack} className="grid grid-cols-2 text-gray-400 hover:text-white hover:bg-gray-900 py-4 px-2 pr-3 cursor-pointer">
+    <div
+      onClick={playTrack}
+      className="grid grid-cols-2 text-gray-400 hover:text-white hover:bg-gray-900 py-4 px-2 pr-3 cursor-pointer"
+    >
       <div className="flex items-center space-x-4">
         <p className="mr-4">{order + 1}</p>
         <Image
@@ -36,7 +39,9 @@ export default function Song({ order, track }) { // get order and track from com
       </div>
       <div className="flex items-center justify-between ml-auto md:ml-0">
         <p className="hidden md:inline">{track.album.name}</p>
-        <p className="text-gray-400">{millisToMinutesAndSeconds(track.duration_ms)}</p>
+        <p className="text-gray-400">
+          {millisToMinutesAndSeconds(track.duration_ms)}
+        </p>
       </div>
     </div>
   );
